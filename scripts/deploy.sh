e4941c951c39da7109095b9956cbd102fdbdfd34
guaguaerhao <2809731342@qq.com>
2020-03-09 14:28:49 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
+# 构想 https://gist.github.com/motemen/8595451
 
+# 基于 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
+# MIT许可 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
+
+# abort the script if there is a non-zero error
 set -e
 
+# 打印当前的工作路径
 pwd
 remote=$(git config remote.origin.url)
 
-echo 'remote is:'$remote
+echo 'remote is: '$remote
 
-# 新建一个发布目录
+# 新建一个发布的目录
 mkdir gh-pages-branch
 cd gh-pages-branch
 # 创建的一个新的仓库
-# 设置发布的用户名和邮箱
+# 设置发布的用户名与邮箱
 git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
 git config --global user.name "$GH_NAME" >/dev/null 2>&1
 git init
@@ -23,13 +29,12 @@ echo 'sitesource is: '$siteSource
 
 # 切换gh-pages分支
 if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
-    git checkout gh-pages
-    # 删除掉旧的文件内容
-    git rm -rf .
+  git checkout gh-pages
+  # 删除掉旧的文件内容
+  git rm -rf .
 else
-    git checkout -orphan gh-pages
+  git checkout --orphan gh-pages
 fi
-
 
 # 把构建好的文件目录给拷贝进来
 cp -a "../${siteSource}/." .
@@ -42,7 +47,7 @@ git add -A
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # 推送文件
 git push --force --quiet origin gh-pages
-# 资源回收，删除临时分支和目录
+# 资源回收，删除临时分支与目录
 cd ..
 rm -rf gh-pages-branch
 
