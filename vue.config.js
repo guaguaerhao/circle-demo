a4820a47bb8c355fe0ac517974f5b153dd0431e2
guaguaerhao <2809731342@qq.com>
2020-03-09 14:36:19 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "circle-demo" : ""
+  publicPath: process.env.NODE_ENV === "production" ? "/circle-demo" : ""
 }
